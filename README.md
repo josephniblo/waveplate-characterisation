da02a117b3fed95b251c96b53cf3ea6af248474f
Joseph Niblo <61010077+josephniblo@users.noreply.github.com>
2025-02-06 14:22:59 +0000

    Clean up dependencies

--- README.md
+++ README.md
@@ -1,18 +1,19 @@
 # README
 
+This script connects to a PM100D Thor Labs Power meter.
+
 ## Requirements
 
-- Power meter
-    - Install the ThorLabs software required to read the power meter
-    - As of 2025-02-05, this is found at https://www.thorlabs.com/software_pages/ViewSoftwarePage.cfm?Code=OPM
-    - Version 6.1
-- libusb
-    - C library to give generic access to USB devices
-    - Download from https://libusb.info/
-    - Version 1.0.27
 - Python dependencies
     - Use [pipenv](https://pipenv.pypa.io/en/latest/) to install python dependencies
-- Copy the libusb-1.0.dll into .venv/Scripts to make it available  
-- NI-VISA
-    - Used by PyUSB
-    - Version 2025 Q1
+- Copy the libusb-1.0.dll into .venv/Scripts to make it available to the script
+
+## Gotchas
+
+- Only one thing can connect to the device at a time - if you are connected in eg. Optical Power Monitor software, it will not show up to the python script.
+
+## Alternative
+
+- Install the ThorLabs software required to read the power meter
+- As of 2025-02-05, this is found at https://www.thorlabs.com/software_pages/ViewSoftwarePage.cfm?Code=OPM, version 6.1
+- This has a nice GUI for interacting with the Power Meter
--- script.py
+++ script.py
@@ -1,74 +1,67 @@
-"""
-PMxxx_SCPI
-Example Date of Creation: 2023-10-09
-Example Date of Last Modification on Github: 2023-10-09
-Version of Python: 3.11
-Version of the Thorlabs SDK used: -
-==================
-Example Description: The example shows how to use SCPI commands in Python with pyvisa
-"""
-
-#Import the PyVISA library to Python.
 import pyvisa
+
+# Known power meter IDs
+PM100D_P0010673 = "USB0::4883::32888::P0010673::0::INSTR"
+
+# Choose the power meter to connect to
+METER_ID = PM100D_P0010673
+
 
 def main():
     rm = None
     device = None
 
-    #Opens a resource manager
-    rm = pyvisa.ResourceManager('@py')
-    # rm = pyvisa.ResourceManager()
+    try:
+        rm = pyvisa.ResourceManager("@py")
+        res_found = rm.list_resources(METER_ID)
 
-    res_found = rm.list_resources('USB0::4883::32888::P0010673::0::INSTR')
-    # res_found = rm.list_resources('ASRL3::INSTR')
+        if len(res_found) == 0:
+            print("No power meter found")
+            raise Exception(
+                "No power meter found. Check you are not connected in another program, eg. Thorlabs Optical Power Meter software."
+            )
 
-    #Opens the connection to the device. The variable instr is the handle for the device.
-    # !!! In the USB number the serial number (P00...) and PID (0x8078) needs to be changed to the one of the connected device.
-    #Check with the Windows DEvice Manager
-   
-    # Connect to the power meter, make it beep, and ask it for its ID
-    print('Connecting to PM100D...')
-    meter = rm.open_resource(res_found[0])
-    meter.read_termination = '\n'
-    meter.write_termination = '\n'
-    meter.timeout = 3000  # ms
+        # Connect to the power meter, make it beep, and ask it for its ID
+        print("Connecting to PM100D...")
+        meter = rm.open_resource(res_found[0])
+        meter.read_termination = "\n"
+        meter.write_termination = "\n"
+        meter.timeout = 3000  # ms
 
-    meter.write('system:beeper')
+        meter.write("system:beeper")
 
-    print('*idn?')
-    print('--> ' + meter.query('*idn?'))
-    
-    # #print the device information
-    # print(instr.query("SYST:SENS:IDN?"))
-    
-    # #turn on auto-ranging
-    # instr.write("SENS:RANGE:AUTO ON")
-    #set wavelength setting, so the correct calibration point is used
-    meter.write("SENS:CORR:WAV 1310")
-    #set units to Watts
-    meter.write("SENS:POW:UNIT W")
-    #set averaging to 1000 points
-    meter.write("SENS:AVER:1000")
+        print("*idn?")
+        print("--> " + meter.query("*idn?"))
 
-    #read the power
-    print (meter.query("MEAS:POW?"))
+        # set wavelength setting, so the correct calibration point is used
+        meter.write("SENS:CORR:WAV 1550")
+        # set units to Watts
+        meter.write("SENS:POW:UNIT W")
+        # set averaging to 1000 points
+        meter.write("SENS:AVER:1000")
 
-    #Close device in any case
-    if device is not None:
-        try:
-            device.close()
-        except Exception:
-            pass
+        # read the power
+        print(meter.query("MEAS:POW?"))
 
-    #Close resource manager in any case
-    if rm is not None:
-        try:
-            meter.close()
-        except Exception:
-            pass
+    except Exception as e:
+        print("Error: " + str(e))
+    finally:
+        # Close device in any case
+        if device is not None:
+            try:
+                device.close()
+            except Exception:
+                pass
 
-    #close out session
-    rm.close()
+        # Close resource manager in any case
+        if rm is not None:
+            try:
+                meter.close()
+            except Exception:
+                pass
+            # close out session
+            rm.close()
+
 
 if __name__ == "__main__":
     main()
